dad58a095ea1a50922cfa02e17e4108d49273d52
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-03-22 19:48:51 +0200

    Get Meeting Data

--- index.php
+++ index.php
@@ -11,19 +11,19 @@ $client_secret = 'sxhSdiJGFpIpBlyh60TqW0E14pSh8Zim';
 $zoom = new ZoomIntegrationService($account_id, $client_id, $client_secret);
 
 // ISO 8601 date format
-$start_time = date('Y-m-d\TH:i:s', strtotime('2024-11-04 17:00'));
+// $start_time = date('Y-m-d\TH:i:s', strtotime('2024-11-04 17:00'));
 
-$meetingData = [
-    'topic' => 'Update Meeting',
-    'type' => 2,
-    'start_time' => $start_time,
-    'duration' => 30,
-    'timezone' => 'UTC',
-    'genda' => 'Discuss Project',
-];
+// $meetingData = [
+//     'topic' => 'Update Meeting',
+//     'type' => 2,
+//     'start_time' => $start_time,
+//     'duration' => 30,
+//     'timezone' => 'UTC',
+//     'genda' => 'Discuss Project',
+// ];
 
 $id = 86175169848;
 
 echo '<pre>';
-echo print_r($zoom->updateMeeting($id , $meetingData));
+echo print_r($zoom->getMeeting($id));
 echo '</pre>';
