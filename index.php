5631f3741cf88839a61c4b0eae08adf0d7b4a8f1
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2025-03-22 19:44:44 +0200

    Update Meeting

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $zoom = new ZoomIntegrationService($account_id, $client_id, $client_secret);
 $start_time = date('Y-m-d\TH:i:s', strtotime('2024-11-04 17:00'));
 
 $meetingData = [
-    'topic' => 'Create Meeting',
+    'topic' => 'Update Meeting',
     'type' => 2,
     'start_time' => $start_time,
     'duration' => 30,
@@ -25,5 +25,5 @@ $meetingData = [
 $id = 86175169848;
 
 echo '<pre>';
-echo print_r($zoom->createMeeting($meetingData));
+echo print_r($zoom->updateMeeting($id , $meetingData));
 echo '</pre>';
